b77f41c490e471ac0951c2adc1168978f807b6dd
Kevin Duggan <kevinduggan95@gmail.com>
2023-03-20 11:48:29 -0500

    zoo/animal lab

--- lib/animal.py
+++ lib/animal.py
@@ -1,2 +1,15 @@
 class Animal:
-    pass
+    all = []
+    def __init__(self, species, weight, nickname, daZoo ):
+        self.species = species
+        self.weight = weight
+        self.nickname = nickname
+        self.zoo = daZoo
+        Animal.all.append( self )
+
+ # should take in an animal's species as an argument and return an list of 
+    # all the animals, which are of that species
+
+    @classmethod
+    def find_by_species( cls, species):
+        return [mate.nickname for mate in cls.all if mate.species == species]
--- lib/zoo.py
+++ lib/zoo.py
@@ -1,4 +1,50 @@
 from lib.animal import Animal
 
 class Zoo:
-    pass
+    all = []
+    def __init__( self, name, location):
+        self.name = name 
+        self.location =location 
+        Zoo.all.append( self )
+
+# should return all the animals that a specific instance of a zoo has.
+
+    @property
+    def animals( self ):
+        newInDaZoo = []
+        for mate in Animal.all:
+            newInDaZoo.append( mate )
+        return newInDaZoo
+    
+# should return an list of all the species (as strings) of 
+    # the animals in the zoo. However, if you have two dogs, it should only 
+    # return one "Dog" string (aka an unique list).
+
+    @property
+    def animal_species( self ):
+        return list( { mate.species for mate in self.animals })
+    
+ # should take in an animal's species as an argument and return an list of 
+    # all the animals in that zoo, which are of that species.
+
+    
+    def find_by_species( self, species ):
+        return [mate for mate in self.animals if mate.species == species]
+    
+
+ # should return an list of all the nicknames of animals that a specific 
+    # instance of a zoo has.
+
+
+    @property
+    def animal_nicknames( self ):
+        return [ mate.nickname for mate in self.animals ]
+    
+    
+  # should take in a location as an argument and return an list of all the 
+    # zoos within that location.
+    
+    @classmethod
+    def find_by_location( cls, daLocation):
+        return [ z for z in cls.all if z.location == daLocation ]
+
